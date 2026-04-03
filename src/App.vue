682c17e0107f71bfdf63b4b3e7cc213ee2dc7137
susanacst <susanacst098@gmail.com>
2026-04-03 15:17:12 -0600

    feat: about section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import Header from './components/header.vue';
 import Footer from './components/footer.vue';
+import About from './components/about.vue';
 
 </script>
 
 <template>
   <Header/>
+  <About/>
   <Footer/>
+  
 </template>
 
---
+++ src/components/about.vue
@@ -0,0 +1,37 @@
+<template>
+  <section id="about">
+    <div class="welcome-paragraph">
+      <h3>Hey World, I'm Susan!</h3>
+
+      <div class="profile-img">
+        <picture>
+          <img :src="profileImg" alt="profile">
+        </picture>
+      </div>
+
+      <p class="paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+    </div>
+
+    <div class="contact-icons">
+      <a href="https://github.com/susanacst"><i class="fa-brands fa-github"></i></a>
+      <a href="https://www.linkedin.com/in/susanasct/"><i class="fa-brands fa-linkedin-in"></i></a>
+      <a href="mailto:susanasct098@gmail.com"><i class="fa-regular fa-envelope"></i></a>
+    </div>
+
+    <div class="main-container-buttons">
+      <a href="https://drive.google.com/...">
+        <button class="primary-button">
+          <i class="fa-regular fa-star"></i>
+          download cv
+          <i class="fa-regular fa-star"></i>
+        </button>
+      </a>
+    </div>
+  </section>
+</template>
+
+<script setup>
+  import profileImg from '../assets/img-profile.jpg'  
+</script>
