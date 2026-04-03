8011e5d622f905e169ab91079aabc94392d3725e
susanacst <susanacst098@gmail.com>
2026-04-03 15:23:01 -0600

    feat: projects section

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Header from './components/header.vue';
 import Footer from './components/footer.vue';
 import About from './components/about.vue';
+import Proyects from './components/projects.vue';   
 
 </script>
 
 <template>
   <Header/>
   <About/>
+  <Proyects/>
   <Footer/>
   
 </template>
---
+++ src/components/projects.vue
@@ -0,0 +1,42 @@
+<script setup>
+const projects = [
+  {
+    title: "Project title",
+    description: "Lorem ipsum dolor sit amet...",
+    image: "src/assets/captura-ejemplo.jpg",
+    github: "https://github.com/susanacst",
+    demo: "https://proyecto-ejemplo.com"
+  }
+]
+</script>
+
+<template>
+  <section id="proyects">
+    <h2>Proyects</h2>
+
+    <div class="card-proyect-container">
+      <div class="card" v-for="(project, index) in projects" :key="index">
+        
+        <div class="img">
+          <img :src="project.image" alt="project">
+        </div>
+
+        <div class="text">
+          <p class="h3">{{ project.title }}</p>
+
+          <div class="info-card">
+            <p class="summary-proyect">
+              {{ project.description }}
+            </p>
+
+            <div class="buttons-proyect-container">
+              <a :href="project.github"><i class="fa-brands fa-github"></i></a>
+              <a :href="project.demo"><i class="fa-solid fa-globe"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
